68e50018cc78d8bfe288f8bbb8c20e4a6283d205
T Brichetto <TaraPersonal@MacBook-Air.local>
2020-10-25 16:38:16 -0700

    Product Routes Complete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,10 +7,11 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
+    attributes: ['id', 'category_name'],
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        attributes: ['id', 'product_name', 'price', 'stock']
       }
     ]
   })
